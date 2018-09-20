002bad46b466fae77274e5fff150a9dd7bae1ff1
Jordan Rand <jorrand@gmail.com>
2018-09-20 11:38:33 -0600

    add redux

---
+++ config/initializers/devise.rb
@@ -0,0 +1,4 @@
+
+Devise.setup do |config|
+  config.secret_key = '546763337cc7ec56d52fe303e4b112ac1db20e0f035a9d08ff8e5eb4396dcc7848cca751dd0246e067fa295c04c3a2d185fed106dafff9aaa76d6cf29dfa0aec'
+end
